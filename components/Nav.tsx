085cfaa4d894870d2be07b08c6c12daf526cc493
Wildanzr <graita.sukma@gmail.com>
2022-07-23 09:08:18 +0700

    feat: nav dropdown

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -29,8 +29,21 @@ const Nav = () => {
 
       <div className="flex-none font-poppins font-medium hidden lg:block">
         <div className="flex flex-row">
-          <a href='#home' id='scroller' className="text-lg px-5 tracking-wider hover:font-semibold duration-100 uppercase">Home</a>
-          <a href='#projects' id='scroller' className="text-lg px-5 tracking-wider hover:font-semibold duration-100 uppercase">Project</a>
+          <a href='#home' className="text-lg px-5 tracking-wider hover:font-semibold duration-100 uppercase">Home</a>
+          <div className="dropdown dropdown-hover cursor-pointer">
+            <label className="text-lg px-5 tracking-wider hover:font-semibold duration-100 uppercase">Project</label>
+            <ul className="dropdown-content p-2 bg-white shadow w-52">
+              <li className='py-2'>
+                <a className="text-base cursor-pointer hover:font-semibold duration-100">Sakinah Residence</a>
+              </li>
+              <li className='py-2'>
+                <a className="text-base cursor-pointer hover:font-semibold duration-100">Griya Mega Sakinah</a>
+              </li>
+              <li className='py-2'>
+                <a className="text-base cursor-pointer hover:font-semibold duration-100">Mutiara Sakinah</a>
+              </li>
+            </ul>
+          </div>
           <a href='#testimonials' id='scroller' className="text-lg px-5 tracking-wider hover:font-semibold duration-100 uppercase">About</a>
           <a href='#contacts' id='scroller' className="text-lg px-5 tracking-wider hover:font-semibold duration-100 uppercase">Contact</a>
         </div>
--- components/Wrapper.tsx
+++ components/Wrapper.tsx
@@ -18,9 +18,25 @@ const Wrapper = (props: LayoutProps) => {
       </div>
       <div className="drawer-side">
         <label htmlFor="my-drawer-3" className="drawer-overlay"></label>
-        <div className="flex flex-col font-poppins font-medium p-4 overflow-y-auto w-40 bg-white">
+        <div className="flex flex-col font-poppins font-medium p-4 overflow-y-auto w-56 bg-white">
           <a className="text-lg py-3 tracking-wider uppercase">Home</a>
-          <a className="text-lg py-3 tracking-wider uppercase">Project</a>
+          <div className="collapse">
+            <input type="checkbox"/>
+            <div className="collapse-title text-lg py-3 tracking-wider uppercase" style={{ paddingLeft: 0 }}>
+              Project
+            </div>
+            <ul className="collapse-content bg-white shadow">
+              <li className='py-2'>
+                <a className="text-base cursor-pointer hover:font-semibold duration-100">Sakinah Residence</a>
+              </li>
+              <li className='py-2'>
+                <a className="text-base cursor-pointer hover:font-semibold duration-100">Griya Mega Sakinah</a>
+              </li>
+              <li className='py-2'>
+                <a className="text-base cursor-pointer hover:font-semibold duration-100">Mutiara Sakinah</a>
+              </li>
+            </ul>
+          </div>
           <a className="text-lg py-3 tracking-wider uppercase">About</a>
           <a className="text-lg py-3 tracking-wider uppercase">Contact</a>
         </div>
